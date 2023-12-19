36c72e8f49095ebfbd9f8b0ff6e96db1a10e6182
milimoe <mili@wrss.org>
2023-12-19 19:49:51 +0800

    添加项目文件。

---
+++ OfficialMap16x16.cs
@@ -0,0 +1,19 @@
+﻿namespace Milimoe.FunGame.GameMap.OfficialStandard
+{
+    public class OfficialMap16x16 : Core.Library.Common.Addon.GameMap
+    {
+        public override string Name => "OfficialMap16x16";
+
+        public override string Description => "宽16格高16格，尺寸为32的地图";
+
+        public override string Version => Core.Library.Constant.FunGameInfo.FunGame_Version;
+
+        public override string Author => Core.Library.Constant.FunGameInfo.FunGame_CopyRight;
+
+        public override float Width => 16;
+
+        public override float Height => 16;
+
+        public override float Size => 32;
+    }
+}
