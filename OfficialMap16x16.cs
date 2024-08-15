39b4f27014fff5ef58194acc21e9239721c1f11d
milimoe <mili@wrss.org>
2024-08-16 00:58:46 +0800

    适配新的Core DLL

--- OfficialMap16x16.cs
+++ OfficialMap16x16.cs
@@ -4,12 +4,14 @@
     {
         public override string Name => "OfficialMap16x16";
 
-        public override string Description => "宽16格高16格，尺寸为32的地图";
+        public override string Description => "长16格，宽16格，高16格，尺寸为32的立方体地图";
 
         public override string Version => Core.Library.Constant.FunGameInfo.FunGame_Version;
 
         public override string Author => Core.Library.Constant.FunGameInfo.FunGame_CopyRight;
 
+        public override float Length => 16;
+        
         public override float Width => 16;
 
         public override float Height => 16;
